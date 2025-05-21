5ae88ed4d80cd84ccb50111a779c43ff5194b7e2
ismontana <ismaotsss@gmail.com>
2025-05-21 11:26:37 -0600

    Modificacion de app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@
 export default function App() {
 
   return (
-    <div>
-      <h1>Sitio de info</h1>
+    <div className="flex justify-center bg-gradient-to-r from-blue-500 to-pink-500 h-screen">
+      <h1 className="text-blue-600 text-xl font-bold hover:animate-pulse">Sitio de info</h1>
     </div>
   )
 }
